6010fed9b87d0479ba9d29d32505eeaa7205994b
adriancuadros <adrian@reservamos.mx>
2018-07-26 18:51:21 -0500

    Adds railsproject

---
+++ lovemodels/config/initializers/constants.rb
@@ -0,0 +1,83 @@
+MOVIES = [
+  {
+    name: "Mission Imposible: Fallout",
+    genre: "Action",
+    length: 147,
+    review: 4.5
+  },
+  {
+    name: "Happy Gilmore",
+    genre: "Comedy",
+    length: 92,
+    review: 3.8
+  },
+  {
+    name: "Saving Private Ryan",
+    genre: "Comedy",
+    length: 169,
+    review: 4.8
+  }
+]
+
+TRAINERS = [
+  {
+    name: 'Ash Ketchum',
+    town: 'Pallet',
+    age: 10
+  },
+  {
+    name: 'Brock',
+    town: 'Pewter',
+    age: 11
+  },
+  {
+    name: 'Misty',
+    town: 'Cerulean',
+    age: 10
+  }
+]
+
+POKEMON = [
+  {
+    name: 'Charmander',
+    category: 'Fire'
+  },
+  {
+    name: 'Bulbasaur',
+    category: 'Plant'
+  },
+  {
+    name: 'Squirtle',
+    category: 'Water'
+  }
+]
+
+ATTACK = [
+  {
+    name: 'Ember',
+    category: 'Fire'
+  },
+  {
+    name: 'Water Gun',
+    category: 'Water'
+  },
+  {
+    name: 'Vine Whip',
+    category: 'Plant'
+  },
+  {
+    name: 'Headbutt',
+    category: 'Normal'
+  }
+]
+
+BADGE = [
+  {
+    name: 'Boulder',
+    gym: 'Pewter'
+  },
+  {
+    name: 'Cascade',
+    gym: 'Cerulean'
+  }
+]
